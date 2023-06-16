b777f6aa5dc78f274ec1e98571ae082d17b9387e
Mezeo-22 <ktoto.tam2005@mail.ru>
2023-06-16 11:22:41 +0900

    бд на месте

---
+++ database/migrations/2023_06_16_015715_create_documents_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->date('docdate');
+            $table->string('name');
+            $table->unsignedBigInteger('typeid');
+            $table->string('pathname');
+
+            $table->index('typeid');
+            $table->foreign('typeid')->on('doc_types')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
